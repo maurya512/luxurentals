61a05bd9794461c23d15d58604d6003710cd5d31
maurya512 <patelmaurya0512@gmail.com>
2021-03-01 22:59:58 -0500

    added useState to toggle the sidebar menu

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // import components
 import Navbar from '../Navbar'
+import Sidebar from '../Sidebar';
 import { HeroContainer, HeroContent, HeroH1, HeroItems, HeroBtn, HeroP } from './HeroElement';
 function Hero() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <HeroContainer>
-            <Navbar />
+            <Navbar toggle={toggle} />
+            <Sidebar isOpen={isOpen} toggle={toggle} />
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Renting Luxury Cars Made Easy</HeroH1>
