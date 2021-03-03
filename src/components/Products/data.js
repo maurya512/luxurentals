595a3f810894e954a0f257e0d2ba68f766a6c69c
maurya512 <patelmaurya0512@gmail.com>
2021-03-02 23:16:28 -0500

    Added more products to the database

---
+++ src/components/Products/data.js
@@ -0,0 +1,60 @@
+import prd1 from '../../images/acura.jpg';
+import prd2 from '../../images/aventador.jpg';
+import prd3 from '../../images/bmw.jpg';
+import prd4 from '../../images/db9.jpg';
+import prd5 from '../../images/enzo.jpg';
+import prd9 from '../../images/mclaren.jpg';
+
+
+// array of objects for data to display on the website
+export const productData = [
+    {
+        img: prd1,
+        alt: 'Acura',
+        name: 'Acura NSX',
+        desc: 'THAT FEELING OF EMOTIONAL CONNECTEDNESS reflects both the power of dreams and driven creativity that fuels the craft of engineering. Reignited in a new generation of engineers, that connection brought forth a new vision: to honor the car’s lineage by creating a boldly evolved NSX for a new century.',
+        price: '$599/day',
+        button: 'Book Now'
+    }, 
+    {
+        img: prd2,
+        alt: 'Lambo Aventador',
+        name: 'Lamborghini Aventador',
+        desc: 'Forget the idea of performance you are used to. The Aventador Coupé has been engineered to revolutionize this concept and establish a new benchmark in the segment of super sports cars and beyond. This is a true supercar legend in the making, which combines the tradition of the Lamborghini brand with a level of innovation which takes the House of the Raging Bull to hitherto unexplored territory',
+        price: '$1099/day',
+        button: 'Book Now'
+    }
+    ,{
+        img: prd3,
+        alt: 'BMW',
+        name: 'BMW M3',
+        desc: 'Packed with power and made to maneuver, BMW M excels on the track and in the street. And after four decades of hair-raising thrills, it’s still pushing the limits. Grip the wheel of any of our M vehicles – from coupes and convertibles to Sports Activity Vehicles® – and elevate your adrenaline rushes to a higher level.',
+        price: '$500/day',
+        button: 'Book Now',
+    }, 
+    {
+        img: prd4,
+        alt: 'Aston Martin DB 9',
+        name: 'Aston Martin DB 9',
+        desc: 'Bond..... James Bond',
+        price: '$899/day',
+        button: 'Book Now',
+    }, 
+    {
+        img: prd5,
+        alt: 'Enzo',
+        name: 'Ferrari Enzo',
+        desc: 'Over the years Ferrari has introduced a series of supercars which have represented the very pinnacle of the company’s technological achievements transferred to its road cars. These include the GTO, F40 and F50.',
+        price: '$1199/day',
+        button: 'Book Now',
+    },
+    {
+        img: prd9,
+        alt: 'McLaren',
+        name: 'McLaren P1',
+        desc: 'The McLaren P1 is a limited-production mid-engine plug-in hybrid sports car produced by British automobile manufacturer McLaren Automotive. ... It is considered by the automotive press to be the successor to the McLaren F1, utilising hybrid power and Formula One technology, but does not have the same three-seat layout.',
+        price: '$999/day',
+        button: 'Book Now',
+
+    }
+]
