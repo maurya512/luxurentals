ad79223568de1d550937523b9c8307148d9e3e9d
maurya512 <patelmaurya0512@gmail.com>
2021-03-02 23:16:51 -0500

    Feature Component

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FeatureContainer, FeatureButton } from './FeatureElement';
+
+const Feature = () => {
+    return (
+        <FeatureContainer>
+            <h1>Ride of The Day</h1>
+            <p> Nissan Gt-R Nismo Package</p>
+            <FeatureButton>Ride it while it lasts</FeatureButton>
+        </FeatureContainer>
+    )
+}
+
+export default Feature
